0fecdac68ebf143cab154adf3a13003269ba98be
Lilian Hammache <fayzerdev@gmail.com>
2025-02-12 22:54:39 +0100

    🚧 Make Frontend for form

--- src/Form/WardrobeItemType.php
+++ src/Form/WardrobeItemType.php
@@ -38,7 +38,7 @@ class WardrobeItemType extends AbstractType
             ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
         ;
     }
