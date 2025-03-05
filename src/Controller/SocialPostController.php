c6b34cd269e7a0416a42506d7bd461555fbd5589
Lilian Hammache <fayzerdev@gmail.com>
2025-03-05 20:41:13 +0100

    ✨ Feature Like social post

--- src/Controller/SocialPostController.php
+++ src/Controller/SocialPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\PostLike;
 use App\Entity\SocialPost;
 use App\Form\SocialPostType;
 use App\Repository\SocialPostRepository;
@@ -11,6 +12,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 #[Route('/social/post')]
 final class SocialPostController extends AbstractController
@@ -99,5 +101,39 @@ final class SocialPostController extends AbstractController
         }
 
         return $this->redirectToRoute('app_social_post_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}/like', name: 'app_social_post_like', methods: ['POST'])]
+    public function like(SocialPost $socialPost, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+    
+        $likeRepository = $entityManager->getRepository(PostLike::class);
+    
+        // Vérifie si l'utilisateur a déjà liké le post
+        $existingLike = $likeRepository->findOneBy([
+            'userId' => $user,
+            'postId' => $socialPost,
+        ]);
+    
+        if ($existingLike) {
+            $entityManager->remove($existingLike);
+            $entityManager->flush();
+            $this->addFlash('success', 'Like retiré.');
+        } else {
+            $like = new PostLike();
+            $like->setUserId($user);
+            $like->setPostId($socialPost);
+            $like->setCreatedAt(new \DateTimeImmutable());
+    
+            $entityManager->persist($like);
+            $entityManager->flush();
+            $this->addFlash('success', 'Post liké !');
+        }
+    
+        return $this->redirectToRoute('app_social_post_index');
     }
 }
